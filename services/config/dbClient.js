434ccac40bc0bbbf1a5faef43c6dc87f1f13c79b
nessd00dle <reyesvanessa940@gmail.com>
2026-05-14 17:42:06 -0600

    mongo configurado

--- services/config/dbClient.js
+++ services/config/dbClient.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb://127.0.0.1:27017/CardDial");
+        const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB conectado:", conn.connection.host);
     } catch (error) {
